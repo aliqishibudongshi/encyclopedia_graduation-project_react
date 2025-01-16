469b2336d3836d9b260ee9af7b3c5c12fd711ec9
sillyroedeerpig <13581452+sillyroedeerpig@user.noreply.gitee.com>
2025-01-16 10:16:49 +0800

    remove Provider and PersistGate from index.js to App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,27 @@
 import { HashRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import IndexRouter from './router/IndexRouter';
 import theme from './theme';
 
 const App = () => {
   return (
-    
-      <HashRouter>
-        <ConfigProvider
-          theme={{
-            token: { colorPrimary: theme.colorPrimary },
-            components: { Button: { colorPrimary: theme.Button?.colorPrimary } },
-          }}
-        >
-          <IndexRouter />
-        </ConfigProvider>
-      </HashRouter>
-    
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <HashRouter>
+          <ConfigProvider
+            theme={{
+              token: { colorPrimary: theme.colorPrimary },
+              components: { Button: { colorPrimary: theme.Button?.colorPrimary } },
+            }}
+          >
+            <IndexRouter />
+          </ConfigProvider>
+        </HashRouter>
+      </PersistGate>
+    </Provider>
   );
 };
 
--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
 import './index.css';
 import App from './App';
 
@@ -11,10 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode >
 );
