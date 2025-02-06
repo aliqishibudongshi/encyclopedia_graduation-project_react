93eb41bb5deb86da9e41b820f66a0600ae249f2c
sillyroedeerpig <13581452+sillyroedeerpig@user.noreply.gitee.com>
2025-02-06 11:30:13 +0800

    upgrade

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,6 +1,9 @@
-// jest.config.js
 module.exports = {
     testEnvironment: 'jsdom',
+    moduleDirectories: ["node_modules", "src"],
+    moduleNameMapper: {
+        '\\.(css|less)$': 'identity-obj-proxy' // 处理样式文件
+    },
     transform: {
         '^.+\\.[jt]sx?$': 'babel-jest',
         '\\.(css|less|scss|sass)$': 'jest-transform-css'
@@ -8,5 +11,7 @@ module.exports = {
     transformIgnorePatterns: [
         'node_modules/(?!(axios)/)' // 允许转换axios模块
     ],
-    testTimeout: 10000
+    testTimeout: 10000,
+
+    testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"], // Ensure Jest finds test files
 };
